4b3208730f5201e30209ed96af7b0b2b7b0868f9
bertekg <bartek.grywalski@gmail.com>
2023-09-26 21:00:25 +0200

    L12 Konfigurowanie modelu

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -12,6 +12,15 @@ public class MyBoardsContext : DbContext
     public DbSet<Address> Addresses { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
+        modelBuilder.Entity<WorkItem>(eb =>
+        {
+            eb.Property(wi => wi.State).IsRequired();
+            eb.Property(wi => wi.Area).HasColumnType("varchar(200)");
+            eb.Property(wi => wi.IterationPath).HasColumnName("Iteration_Path");
+            eb.Property(wi => wi.Efford).HasColumnType("decimal(5,2)");
+            eb.Property(wi => wi.EndDate).HasPrecision(3);
+            eb.Property(wi => wi.Activity).HasMaxLength(200);
+            eb.Property(wi => wi.RemaningWork).HasPrecision(14, 2);
+        });
     }
 }
