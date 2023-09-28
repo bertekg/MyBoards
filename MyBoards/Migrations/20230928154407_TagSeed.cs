bf3c754a81ba093a5d26b3f168c774a457be5783
bertekg <bartek.grywalski@gmail.com>
2023-09-28 17:50:57 +0200

    L28 Zadanie praktyczne: seedowanie

---
+++ MyBoards/Migrations/20230928154407_TagSeed.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    public partial class TagSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Tags",
+                column: "Value",
+                value: "Web");
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                column: "Value",
+                value: "UI");
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                column: "Value",
+                value: "Desktop");
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                column: "Value",
+                value: "API");
+
+            migrationBuilder.InsertData(
+                table: "Tags",
+                column: "Value",
+                value: "Service");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Value",
+                keyValue: "Service");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Value",
+                keyValue: "API");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Value",
+                keyValue: "Desktop");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Value",
+                keyValue: "UI");
+
+            migrationBuilder.DeleteData(
+                table: "Tags",
+                keyColumn: "Value",
+                keyValue: "Web");
+        }
+    }
+}
